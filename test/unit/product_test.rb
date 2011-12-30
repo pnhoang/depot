5895f1beb15d98e515819c0b37925b27ebdf4022
Hoang Pham <pnhoang@gmail.com>
2011-12-30 14:27:12 +0100

    Add unit test: product price must be positive

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-    test "product attributes must not be empty" do
+  test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
     assert product.errors[:title].any?
@@ -9,4 +9,24 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
   end
+  
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title",
+    description: "yyy",
+    image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+    product.errors[:price].join('; ')
+    
+    product.price = 0
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+    product.errors[:price].join('; ')
+    
+    product.price = 1
+    assert product.valid?
+  end
+  
+  
 end
