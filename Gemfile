36360e37d2d627abdd8391b6d7467733cbe72b4f
Hoang Pham <pnhoang@gmail.com>
2014-03-18 13:34:30 +0100

    initial commit

--- Gemfile
+++ Gemfile
@@ -22,10 +22,13 @@ gem 'jquery-rails'
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
 # Use unicorn as the web server
-# gem 'unicorn'
+gem 'unicorn'
 
 # Deploy with Capistrano
-# gem 'capistrano'
+gem 'capistrano'
+gem 'rvm-capistrano'
+
+gem 'dotenv-rails'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = '068605091eab388035b25cb9990085cf047bbeb1bf1ec3a643e7f13fa638cc4f9b0d923c1bd41117032f559a7b0ca095bc0b6c3e76fe854b4f3f47c115718983'
+Depot::Application.config.secret_token = ENV['SECRET_TOKEN']
